8983b60198d20c721ce79764c953299a7fe7a01c
kingma-sbw <kingma@sbw-media.ch>
2023-02-10 10:34:51 +0100

    add convert and forms

--- __unzip.php
+++ __unzip.php
@@ -1,5 +1,6 @@
 <?php
-  
+
+if( 'go'=== ($_POST['action']??false)) {
 $zip = new ZipArchive;
   
 // Zip File Name
@@ -12,3 +13,26 @@ if ($zip->open('archiv-20220901-154057.zip') === TRUE) {
 } else {
     echo 'Unzipped Process failed';
 }
+exit(0);
+}?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Restore archive</title>
+</head>
+<body>
+    <h1>Restore archive</h1>
+    <form method="post">
+        <label><select name="filename">
+            <?php
+            foreach(glob('*.zip') as $filename) {
+                printf('<option value="%1$s">%1$s', $filename);
+            }
+            ?>
+        </select></label>
+        <input type="submit" name="action" value="go">
+    </form>
+</body>
+</html>
--- __zip.php
+++ __zip.php
@@ -7,20 +7,27 @@ class HZip
    * @param ZipArchive $zipFile
    * @param int $exclusiveLength Number of text to be exclusived from the file path.
    */
-  private static function folderToZip($folder, &$zipFile, $exclusiveLength) {
+  private static function folderToZip($folder, &$zipFile, $exclusiveLength, $convert_function)
+  {
+
     $handle = opendir($folder);
     echo $folder . '<br>' . PHP_EOL;
     while (false !== $f = readdir($handle)) {
-      if ($f != '.' && $f != '..') {
+      if (($f !== '.')
+        && ($f !== '..')
+      ) {
         $filePath = "$folder/$f";
         // Remove prefix from file path before add to zip.
         $localPath = substr($filePath, $exclusiveLength);
-        if (is_file($filePath)) {
-          $zipFile->addFile($filePath, $localPath);
+        if (is_file($filePath)
+          && ((pathinfo($filePath)['extension']??'') !== 'zip')
+          && ($f !== basename(__FILE__))
+        ) {
+          $zipFile->addFile($filePath,$convert_function($localPath) );
         } elseif (is_dir($filePath)) {
           // Add sub-directory.
           $zipFile->addEmptyDir($localPath);
-          self::folderToZip($filePath, $zipFile, $exclusiveLength);
+          self::folderToZip($filePath, $zipFile, $exclusiveLength, $convert_function);
         }
       }
     }
@@ -35,7 +42,7 @@ class HZip
    * @param string $sourcePath Path of directory to be zip.
    * @param string $outZipPath Path of output zip file.
    */
-  public static function zipDir($sourcePath, $outZipPath)
+  public static function zipDir($sourcePath, $outZipPath, $convert_function)
   {
     $start = microtime(true);
 
@@ -46,15 +53,43 @@ class HZip
     $z = new ZipArchive();
     $z->open($outZipPath, ZIPARCHIVE::CREATE);
     $z->addEmptyDir($dirName);
-    self::folderToZip($sourcePath, $z, strlen("$parentPath/"));
+    self::folderToZip($sourcePath, $z, strlen("$parentPath/"), $convert_function);
     $z->close();
 
     $end = microtime(true);
-    printf( '%0.6f</br>', $end - $start );
+    printf('Done in %0.6f sec.</br>', $end - $start);
   }
 }
 
-$host = str_replace('.', '_', $_SERVER['HTTP_HOST'] );
-$date = date('Ymd-His');
+if ("go" === ($_POST['action']?? false)) {
+  $host = str_replace('.', '_', $_SERVER['HTTP_HOST']);
+  $date = date('Ymd-His');
+  if( $_POST['encode']??false )
+    $convert_function = fn($localPath) => iconv("CP858//IGNORE", "UTF-8", $localPath);
+  else
+    $convert_function = fn($localPath) => $localPath;
 
-HZip::zipDir( './', "./archiv-$host-$date.zip" );
+  HZip::zipDir('./', "./archiv-$host-$date.zip", $convert_function);
+  exit(0);
+}
+if ($_POST['action'] ?? true)
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Create Archive</title>
+</head>
+
+<body>
+  <h1>Create Archive</h1>
+  <form method="post">
+    <label>Windows encode?<input type="checkbox" name="encode" id="encode"></label>
+    <label><input type="submit" name="action" value="go"></label>
+  </form>
+</body>
+
+</html>
