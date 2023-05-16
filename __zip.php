ff1dbc8c721d5a6ecebb0210a10ceadf8c38fa99
kingma-sbw <kingma@sbw-media.ch>
2023-05-16 09:33:12 +0200

    Use DirectoryIterator

--- __zip.php
+++ __zip.php
@@ -1,75 +1,72 @@
 <?php
-class HZip
+class MakeZip
 {
+  private ZipArchive $zipArchive;
+  private int $startPathLength; // chars to remove from the start for the stored entity
+
+  public function __construct(
+    string $zipArchivename,
+    public readonly string $startPath,
+    public readonly mixed $convert_function,
+  )
+  {
+    $this->zipArchive = new \ZipArchive;
+    $this->zipArchive->open($zipArchivename, ZipArchive::CREATE);
+    $this->startPathLength = strlen($this-> startPath);
+
+    $this-> zipDir($startPath);
+  }
+  public function __destruct()
+  {
+    $this-> zipArchive-> close();
+  }
+
   /**
    * Add files and sub-directories in a folder to zip file.
    * @param string $folder
    * @param ZipArchive $zipFile
    * @param int $exclusiveLength Number of text to be exclusived from the file path.
    */
-  private static function folderToZip($folder, &$zipFile, $exclusiveLength, $convert_function)
+  private function zipDir($folder)
   {
-
-    $handle = opendir($folder);
     echo $folder . '<br>' . PHP_EOL;
-    while (false !== $f = readdir($handle)) {
-      if (($f !== '.')
-        && ($f !== '..')
-      ) {
-        $filePath = "$folder/$f";
-        // Remove prefix from file path before add to zip.
-        $localPath = substr($filePath, $exclusiveLength);
-        if (is_file($filePath)
-          && ((pathinfo($filePath)['extension']??'') !== 'zip')
-          && ($f !== basename(__FILE__))
-        ) {
-          $zipFile->addFile($filePath,$convert_function($localPath) );
-        } elseif (is_dir($filePath)) {
-          // Add sub-directory.
-          $zipFile->addEmptyDir($localPath);
-          self::folderToZip($filePath, $zipFile, $exclusiveLength, $convert_function);
-        }
+    foreach (new \DirectoryIterator($folder) as $f) {
+      if ($f->isDot()) continue; //skip . ..
+      if ($f->isDir()) {
+        $this-> zipArchive-> addEmptyDir($f->getPathname());
+        $this-> zipDir($f->getPathname());
+
+        continue;
       }
+      if ($f->isFile()) {
+        if ($f->getBasename() === basename(__FILE__)) continue; // skip self 
+        if ($f->getExtension() === 'zip') continue; // skip ZIP files
+
+        $this-> zipArchive ->addFile( substr($f-> getPathname(), $this-> startPathLength) ); // remove './'
+
+        continue;
+      }
+
     }
-    closedir($handle);
-  }
-
-  /**
-   * Zip a folder (include itself).
-   * Usage:
-   *   HZip::zipDir('/path/to/sourceDir', '/path/to/out.zip');
-   *
-   * @param string $sourcePath Path of directory to be zip.
-   * @param string $outZipPath Path of output zip file.
-   */
-  public static function zipDir($sourcePath, $outZipPath, $convert_function)
-  {
-    $start = microtime(true);
-
-    $pathInfo = pathInfo($sourcePath);
-    $parentPath = $pathInfo['dirname'];
-    $dirName = $pathInfo['basename'];
-
-    $z = new ZipArchive();
-    $z->open($outZipPath, ZIPARCHIVE::CREATE);
-    $z->addEmptyDir($dirName);
-    self::folderToZip($sourcePath, $z, strlen("$parentPath/"), $convert_function);
-    $z->close();
-
-    $end = microtime(true);
-    printf('Done in %0.6f sec.</br>', $end - $start);
   }
 }
 
-if ("go" === ($_POST['action']?? false)) {
+if ("go" === ($_POST['action'] ?? false)) {
   $host = str_replace('.', '_', $_SERVER['HTTP_HOST']);
   $date = date('Ymd-His');
-  if( $_POST['encode']??false )
-    $convert_function = fn($localPath) => iconv("CP858//IGNORE", "UTF-8", $localPath);
+  if ($_POST['encode'] ?? false)
+    $convert_function = fn ($localPath) => iconv("CP858//IGNORE", "UTF-8", $localPath);
   else
-    $convert_function = fn($localPath) => $localPath;
+    $convert_function = fn ($localPath) => $localPath;
 
-  HZip::zipDir('./', "./archiv-$host-$date.zip", $convert_function);
+  $start = microtime(true);
+
+  $zip = new \MakeZip("./archiv-$host-$date.zip", './', $convert_function);
+  unset($zip);
+
+  $end = microtime(true);
+  printf('Done in %0.6f sec.</br>', $end - $start);
+
   exit(0);
 }
 if ($_POST['action'] ?? true)
