bedf5fa6fced882922dc401fff7edcd1a4d1faa6
Johannes Kingma <j.kingma@sbw.edu>
2023-06-02 12:04:09 +0200

    Fixes #3

--- __unzip.php
+++ __unzip.php
@@ -1,38 +1,42 @@
 <?php
 
-if( 'go'=== ($_POST['action']??false)) {
-$zip = new ZipArchive;
-  
-// Zip File Name
-if ($zip->open($_POST['filename']) === TRUE) {
-  
+if( 'go' === ( $_POST[ 'action' ] ?? false ) ) {
+  $zip = new ZipArchive;
+
+  // Zip File Name
+  if( $zip->open( $_POST[ 'filename' ] ) === TRUE ) {
+
     // Unzip Path
-    $zip->extractTo('./');
+    $zip->extractTo( './' );
     $zip->close();
     echo 'Unzipped Process Successful!';
-} else {
+  } else {
     echo 'Unzipped Process failed';
-}
-exit(0);
-}?><!DOCTYPE html>
+  }
+  exit( 0 );
+} ?>
+<!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Restore archive</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Restore archive</title>
 </head>
+
 <body>
-    <h1>Restore archive</h1>
-    <form method="post">
-        <label><select name="filename">
-            <?php
-            foreach(glob('*.zip') as $filename) {
-                printf('<option value="%1$s">%1$s', $filename);
-            }
-            ?>
-        </select></label>
-        <input type="submit" name="action" value="go">
-    </form>
+  <h1>Restore archive</h1>
+  <form method="post">
+    <label><select name="filename">
+        <?php
+        foreach( glob( '*.zip' ) as $filename ) {
+          printf( '<option value="%1$s">%1$s', $filename );
+        }
+        ?>
+      </select></label>
+    <input type="submit" name="action" value="go">
+  </form>
 </body>
+
 </html>
--- __zip.php
+++ __zip.php
@@ -1,24 +1,21 @@
-<?php
+<?php declare(strict_types=1);
+phpinfo(1);
 class MakeZip
 {
   private ZipArchive $zipArchive;
   private int $startPathLength; // chars to remove from the start for the stored entity
 
-  public function __construct(
-    string $zipArchivename,
-    public readonly string $startPath,
-    public readonly mixed $convert_function,
-  )
+  public function __construct( string $zipArchiveName, string $startPath )
   {
     $this->zipArchive = new \ZipArchive;
-    $this->zipArchive->open($zipArchivename, ZipArchive::CREATE);
-    $this->startPathLength = strlen($this-> startPath);
+    $this->zipArchive->open( $zipArchiveName, ZipArchive::CREATE );
+    $this->startPathLength = strlen( $startPath );
 
     $this-> zipDir($startPath);
   }
   public function __destruct()
   {
-    $this-> zipArchive-> close();
+    $this->zipArchive->close();
   }
 
   /**
@@ -27,24 +24,27 @@ class MakeZip
    * @param ZipArchive $zipFile
    * @param int $exclusiveLength Number of text to be exclusived from the file path.
    */
-  private function zipDir($folder)
+  private function zipDir( $folder )
   {
     echo $folder . '<br>' . PHP_EOL;
-    foreach (new \DirectoryIterator($folder) as $f) {
-      if ($f->isDot())
+    foreach( new \DirectoryIterator( $folder ) as $f ) {
+      if( $f->isDot() )
         continue; //skip . ..
-      if ($f->isDir()) {
-        if( $f->getExtension() === 'git') continue; // skip .git folder
-        $this-> zipArchive-> addEmptyDir($f->getPathname());
-        $this-> zipDir($f->getPathname());
+      if( $f->isDir() ) {
+        if( $f->getExtension() === 'git' )
+          continue; // skip .git folder
+        $this->zipArchive->addEmptyDir( $f->getPathname() );
+        $this->zipDir( $f->getPathname() );
 
         continue;
       }
-      if ($f->isFile()) {
-        if ($f->getBasename() === basename(__FILE__)) continue; // skip self 
-        if ($f->getExtension() === 'zip') continue; // skip ZIP files
+      if( $f->isFile() ) {
+        if( $f->getBasename() === basename( __FILE__ ) )
+          continue; // skip self 
+        if( $f->getExtension() === 'zip' )
+          continue; // skip ZIP files
 
-        $this-> zipArchive ->addFile( substr($f-> getPathname(), $this-> startPathLength) ); // remove './'
+        $this->zipArchive->addFile( substr( $f->getPathname(), $this->startPathLength ) ); // remove './'
 
         continue;
       }
@@ -53,42 +53,15 @@ class MakeZip
   }
 }
 
-if ("go" === ($_POST['action'] ?? false)) {
-  $host = str_replace('.', '_', $_SERVER['HTTP_HOST']);
-  $date = date('Ymd-His');
-  if ($_POST['encode'] ?? false)
-    $convert_function = fn ($localPath) => iconv("CP858//IGNORE", "UTF-8", $localPath);
-  else
-    $convert_function = fn ($localPath) => $localPath;
+$start = microtime(true);
 
-  $start = microtime(true);
+$host = str_replace( '.', '_', $_SERVER[ 'HTTP_HOST' ] );
+$date = date( 'Ymd-His' );
 
-  $zip = new \MakeZip("./archiv-$host-$date.zip", './', $convert_function);
-  unset($zip);
+$zip = new \MakeZip("./archiv-$host-$date.zip", './');
+unset($zip);
 
-  $end = microtime(true);
-  printf('Done in %0.6f sec.</br>', $end - $start);
 
-  exit(0);
-}
-if ($_POST['action'] ?? true)
-?>
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Create Archive</title>
-</head>
-
-<body>
-  <h1>Create Archive</h1>
-  <form method="post">
-    <label>Windows encode?<input type="checkbox" name="encode" id="encode"></label>
-    <label><input type="submit" name="action" value="go"></label>
-  </form>
-</body>
-
-</html>
+$end = microtime(true);
+printf('Done in %0.6f sec.</br>', $end - $start);
+echo "<a href=\"./archiv-$host-$date.zip\">Download</a>";
